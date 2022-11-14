2355772f0d6b75c329d999d3a4cf66e9038575ec
umutkayabey <94555508+umutkayabey@users.noreply.github.com>
2022-11-14 16:10:53 +0300

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        Scanner sayiGir = new Scanner(System.in);
+        System.out.print("Bir Sayı Girin: ");
+        int number = sayiGir.nextInt();
+        boolean isPrime=true;
+        for (int i=2;i<number;i++)
+        {
+            if(number%i==0)
+            {
+
+                isPrime=false;
+            }
+        }
+        if(number==1){System.out.println("1 Sayısı Asal Değldir"); return;}
+        if(isPrime==true){System.out.println("Asaldır."); }
+        if(isPrime==false){System.out.println("Asal Değildir.");}
+        if(number<2){System.out.println("Geçersiz Sayı");}
+
     }
+
 }
